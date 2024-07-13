edae3bee334873f882161ad77649aad31b94b78b
Thomas Barregren <thomas@kntnt.com>
2024-07-13 08:36:47 +0200

    Update index.php

--- patterns/index.php
+++ patterns/index.php
@@ -6,12 +6,12 @@
  * Inserter: no
  */
 ?>
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70)"><!-- wp:heading {"textAlign":"left","level":1} -->
-<h1 class="wp-block-heading has-text-align-left"><?php esc_html_e('Oops!', 'kntnt-minimal-theme');?></h1>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"80px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:80px"><!-- wp:heading {"level":1} -->
+<h1 class="wp-block-heading"><?php esc_html_e('Oops!', 'kntnt-minimal-theme');?></h1>
 <!-- /wp:heading -->
 
-<!-- wp:paragraph {"align":"left"} -->
-<p class="has-text-align-left"><?php esc_html_e('There is no theme template to display this page. Please let us know so we can fix it. We appreciate your help.', 'kntnt-minimal-theme');?></p>
+<!-- wp:paragraph -->
+<p><?php esc_html_e('There is no theme template to display this page. Please let us know so we can fix it. We appreciate your help.', 'kntnt-minimal-theme');?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
