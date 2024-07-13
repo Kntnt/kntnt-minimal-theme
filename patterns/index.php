4862c2f76363575a2efcf5757c901fe016d9e959
Thomas Barregren <thomas@kntnt.com>
2024-07-13 08:31:55 +0200

    Initial commit

---
+++ patterns/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Title: index
+ * Slug: kntnt-minimal-theme/index
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70)"><!-- wp:heading {"textAlign":"left","level":1} -->
+<h1 class="wp-block-heading has-text-align-left"><?php esc_html_e('Oops!', 'kntnt-minimal-theme');?></h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"left"} -->
+<p class="has-text-align-left"><?php esc_html_e('There is no theme template to display this page. Please let us know so we can fix it. We appreciate your help.', 'kntnt-minimal-theme');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Kntnt Minimal Theme
+
+WordPress theme that is the smallest and simplest possible. You have to create templates and styles yourself.
